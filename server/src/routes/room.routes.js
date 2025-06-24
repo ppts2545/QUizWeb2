b6fa78a1ac28423ed6022652d0b23e522ac0ab4d
Poommy <pongpolpt@gmail.com>
2025-06-24 17:48:10 +0700

    add function on room controller

--- server/src/routes/room.routes.js
+++ server/src/routes/room.routes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const RoomController = require('../controllers/room/RoomController');
 const authMiddleware = require('../middleware/verifyTokenGoogle');
 
+//Create Room
 router.post('/createRoom', authMiddleware, RoomController.createRoom);
 
+//Get Room Thumbnail
+router.get('/rooms', RoomController.createRoomThumbnail);
