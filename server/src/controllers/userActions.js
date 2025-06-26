b339c67bfe62840d55b802bd2a64930c0ec5947a
Poommy <pongpolpt@gmail.com>
2025-06-26 22:28:59 +0700

    change login on email algorithm verify and insert method

--- server/src/controllers/userActions.js
+++ server/src/controllers/userActions.js
@@ -1,14 +1,14 @@
 const { connectMongoDB, connectMySQL } = require('../config/database/connection.js');
 
-exports.createUserIfNotExists = async({email, name, googleId, picture}) => {
+exports.createUserIfNotExists = async({email, name, password}) => {
     const [row] = await connectMySQL.query('SELECT * FROM users WHERE email = ?', [email]);
 
     if(row.length === 0){
         await connectMySQL.query(
-            'INSERT INTO users (email, name, google_id, picture) VALUES (?, ?, ?, ?)',
-            [email, name, googleId || null, picture || null]
+            'INSERT INTO users (email, name, password) VALUES (?, ?, ?)',
+            [email, name, password]
         );
-    } else if (!row[0].googleId){
-        await db.query('UPDATE users SET google_id = ? WHERE email = ?', [googleId, email]);
+    } else if (googleId && !row[0].googleId){
+        await connectMySQL.query('UPDATE users SET google_id = ? WHERE email = ?', [googleId, email]);
     }
 }
